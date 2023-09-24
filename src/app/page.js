9d30944bd28b3285de9bfeb32aa3bf6832f9d424
David Dikman <ddikman@gmail.com>
2023-09-24 14:51:36 +0900

    Added some nice decorations

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div className='flex flex-col justify-center'>
+      <Decoration />
       <h1>Straighten</h1>
       <div className='flex flex-col gap-4'>
         <span><em>Straighten</em> helps you monitor and manage your back curvature.</span>
@@ -13,9 +14,13 @@ export default function Home() {
           <li>Track changes over time</li>
         </ul>
       </div>
-      <div className='py-4'>
+      <div style={{paddingTop: '4rem'}}>
         <Link className="button" href="/sign-in">Start tracking!</Link>
       </div>
     </div>
   )
+}
+
+function Decoration() {
+  return <div style={{ backgroundImage: 'url(/curve.png)', backgroundSize: 'cover', left: '0', right: '0', bottom: '-120px', mixBlendMode: 'multiply', display: 'block', height: '400px', position: 'absolute' }}></div>
 }
