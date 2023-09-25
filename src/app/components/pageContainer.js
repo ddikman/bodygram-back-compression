a419b790b8dddea1578d379e0dba72ef8572e08e
David Dikman <ddikman@gmail.com>
2023-09-25 11:36:51 +0900

    Fixed top margin for devices with notch

--- src/app/components/pageContainer.js
+++ src/app/components/pageContainer.js
@@ -1,5 +1,5 @@
 export default function PageContainer({children, title}) {
-  return <div className="flex flex-col pb-2" style={{ minHeight: '100%' }}>
+  return <div className="flex flex-col pb-2 pt-4" style={{ minHeight: '100%' }}>
     <h1>{title}</h1>
     {children}
   </div>
